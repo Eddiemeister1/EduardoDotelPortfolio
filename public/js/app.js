0b4b0116cf4bd11a66efd772e0c44f46e36af8e6
Eddiemeister1 <42874513+Eddiemeister1@users.noreply.github.com>
2021-01-12 11:05:03 -0500

    Moved app.js to a folder in the public directory

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,7 @@ app.use(express.static('public'));
 
 //make way for some custome css, js, and images
 app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/img', express.static(__dirname + '/public/img'));
 
 var server = app.listen(process.env.PORT || 8080, function() {
