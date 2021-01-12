4e245b482a686e1429d04223489ac9178d528e5a
Eddiemeister1 <42874513+Eddiemeister1@users.noreply.github.com>
2021-01-12 11:30:27 -0500

    Added Videos folder and Resume pdf file

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.static('public'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/img', express.static(__dirname + '/public/img'));
+app.use('/videos', express.static(__dirname + '/public/videos'));
 
 var server = app.listen(process.env.PORT || 8080, function() {
     var port = server.address().port;
